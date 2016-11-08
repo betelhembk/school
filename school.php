5d99caecbc6118502719cc3d3bf254659a12d22e
abebe kebede <abebet@yahoo.com>
2016-11-08 06:54:11 -0800

    Created an school.php file

---
+++ school.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+   <title>School</title>
+   </head>
+   <body>
+   <div id ="wrapper"
+   <form method="POST" action="action.php">
+   <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+    
+  </div>
+   
+   </body>
+   </html>
